bb064f637f6f08fbc18539f34b4466aad87b0173
Owen Sterling <jowen.sterling@csu.fullerton.edu>
2025-02-23 18:32:14 -0800

    Bare Scafolding

--- src/renderer/src/App.svelte
+++ src/renderer/src/App.svelte
@@ -1,26 +1,14 @@
 <script lang="ts">
   import Versions from './components/Versions.svelte'
-  import electronLogo from './assets/electron.svg'
-
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 </script>
 
-<img alt="logo" class="logo" src={electronLogo} />
-<div class="creator">Powered by electron-vite</div>
-<div class="text">
+<div>Powered by <a href="https://electron-vite.org/">electron-vite</a></div>
+
+<div>
   Build an Electron app with
-  <span class="svelte">Svelte</span>
+  <span>Svelte</span>
   and
-  <span class="ts">TypeScript</span>
+  <span>TypeScript</span>
 </div>
-<p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
-<div class="actions">
-  <div class="action">
-    <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">Documentation</a>
-  </div>
-  <div class="action">
-    <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions a11y-missing-attribute-->
-    <a target="_blank" rel="noreferrer" on:click={ipcHandle}>Send IPC</a>
-  </div>
-</div>
+<p>Please try pressing <code>F12</code> to open the devTool</p>
 <Versions />
